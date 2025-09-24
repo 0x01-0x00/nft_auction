e16421b64ed830145cb10e3a92ca15c17517bc9d
zhongtao <ztappleemail@163.com>
2025-09-25 01:56:58 +0800

    feat(hardhat): 集成 hardhat-deploy 和 OpenZeppelin 升级插件- 添加 hardhat-deploy 插件支持- 集成 OpenZeppelin 合约升级功能 - 配置 namedAccounts用于部署账户管理 - 更新 package.json 添加相关依赖项 - 支持可升级合约开发环境配置

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('hardhat-deploy');
+require("@openzeppelin/hardhat-upgrades");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+    solidity: "0.8.28",
+    // namedAccounts 是 hardhat-deploy 插件预定义的配置项名称
+    namedAccounts: {
+        admin: 0,
+        user1: 1,
+        user2: 2,
+    },
 };
